925688d07cb72bf12aad091b23c61fe7b52f2951
letsbreelhere <letsbreelhere@gmail.com>
2022-06-11 00:12:36 -0700

    Half assed explanation

--- 28.hs
+++ 28.hs
@@ -1,6 +1,9 @@
 module Main where
 
 -- Given n, yields the numbers in the spiral of width 2n+1
+-- Basic idea: for the 1x1 case return [1]. For the 3x3 case take the next 4 odd
+-- numbers after 1 (adding 2) up to 9, for 5x5 take every other odd number
+-- after 9 (adding 4) up to 25, etc.
 spiralNumbers :: Int -> [Int]
 spiralNumbers 0 = [1]
 spiralNumbers k =
