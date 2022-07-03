4e76d7df6f8859d7c3a6bc2180330f0ea4a1643c
letsbreelhere <letsbreelhere@gmail.com>
2022-07-02 18:30:28 -0700

    Stack updates

--- 40.hs
+++ 40.hs
@@ -1,12 +1,12 @@
 module Main where
 
-digits :: Int -> [Int]
+digits :: Integer -> [Integer]
 digits n = map (read . (:[])) $ show n
 
-champ :: [Int]
+champ :: [Integer]
 champ = concatMap digits [1..]
 
-cRem :: Int -> (Int, Int)
+cRem :: Integer -> (Integer, Integer)
 cRem = cRem' 0
   where cRem' k n =
           let r = (k+1) * 10^k * 9
@@ -14,14 +14,13 @@ cRem = cRem' 0
                  then (n, k+1)
                  else cRem' (k+1) (n-r)
 
-champAlg :: Int -> Int
+champAlg :: Integer -> Integer
 champAlg n =
   let (m, k) = cRem n
       q = m `div` k
       r = m `mod` k
       n' = 10^(k-1) + q
-   in digits n' !! r
+   in digits n' !! fromIntegral r
 
 main :: IO ()
 main = print . product . map (champAlg . pred . (10^)) $ [0..6]
-
